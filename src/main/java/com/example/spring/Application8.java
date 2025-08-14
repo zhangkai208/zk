7be8690f585ad97d33ff0a6ca4d78702c96efaf8
zksks <2834969129@qq.com>
2025-08-14 15:26:53 +0800

    创建容器，获取bean

--- src/main/java/com/example/spring/Application8.java
+++ src/main/java/com/example/spring/Application8.java
@@ -2,12 +2,15 @@ package com.example.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import javax.sql.DataSource;
 
 public class Application8 {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        DataSource dataSource = context.getBean("dataSource", DataSource.class);
+        ApplicationContext context2 = new FileSystemXmlApplicationContext("C:\\Users\\张恺\\Desktop\\Spring\\src\\main\\resources\\applicationContext.xml");
+        DataSource dataSource = context2.getBean("dataSource", DataSource.class);
         System.out.println(dataSource);
     }
 }
