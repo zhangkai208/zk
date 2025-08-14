4f5993cac030dcc67fbdbe98a4418a6752209ca4
zksks <2834969129@qq.com>
2025-08-14 10:18:07 +0800

    DI依赖注入

--- src/main/java/com/example/spring/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/spring/service/impl/BookServiceImpl.java
@@ -1,15 +1,19 @@
 package com.example.spring.service.impl;
 
 import com.example.spring.dao.BookDao;
-import com.example.spring.dao.impl.BookDaoImpl;
 import com.example.spring.service.BookService;
 
 public class BookServiceImpl implements BookService {
-    private BookDao bookDao = new BookDaoImpl();
+    /*private BookDao bookDao = new BookDaoImpl();*/
+    private BookDao bookDao;
     @Override
     public void save() {
         System.out.println("book service save...");
         bookDao.save();
+    }
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
 
