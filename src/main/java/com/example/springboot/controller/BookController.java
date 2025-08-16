65cc8ffea9ad4d76435fed87573d2e95bf5dc80e
zksks <2834969129@qq.com>
2025-08-16 15:23:27 +0800

    springboot的yml数据读取的三种方式

--- src/main/java/com/example/springboot/controller/BookController.java
+++ src/main/java/com/example/springboot/controller/BookController.java
@@ -1,5 +1,10 @@
 package com.example.springboot.controller;
 
+
+import com.example.springboot.domain.Enterprise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 // @RestController = @Controller + @ResponseBody
 @RequestMapping("/book")
 public class BookController {
+    //1
+    @Value("${server.port}")
+    private Integer port;
+    @Value("${enterprise.subject[0]}")
+    private String subject;
+    @Autowired
+    private Environment env;
 
+    @Autowired
+    private Enterprise enterprise;
     @GetMapping("/{id}")
     public String getById(@PathVariable Integer id){
+        System.out.println("subject = " + subject);
+        System.out.println("port = " + port);
         System.out.println("id = " + id);
+        //2
+        System.out.println("env = " + env.getProperty("enterprise.subject[0]"));
+        System.out.println("enterprise = " + enterprise);
         return "hello";
     }
 }
