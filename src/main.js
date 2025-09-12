2f7fafc06311e8403135f0a499d6b05159ef103e
zksks <2834969129@qq.com>
2025-09-12 14:33:40 +0800

    图标

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import router from './router'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.mount('#app')
--- src/views/Icons.vue
+++ src/views/Icons.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>图标章节</div>
+  <el-row style="margin-bottom: 30px;">
+    <h3>图标</h3>
+    <el-icon><Plus /></el-icon>
+        <el-icon><Edit /></el-icon>
+        <el-icon><Delete /></el-icon>
+  </el-row>
+  <el-row style="margin-bottom: 30px;">
+    <h3>属性</h3>
+        <el-icon size="30" color="red"><Search /></el-icon>
+  </el-row>
+  <el-row style="margin-bottom: 30px;">
+    <h3>按钮</h3>
+    <el-button type="primary">
+            <el-icon><Search /></el-icon>
+            <span> 搜索 </span>
+        </el-button>
+        <el-button type="primary" >
+          <el-icon><Search /></el-icon>
+        </el-button>
+        <el-button type="primary" circle>
+          <el-icon><Search /></el-icon>
+        </el-button>
+  </el-row>
+  <el-row style="margin-bottom: 30px;">
+    <h3>图标按钮组</h3>
+    <el-button-group>
+      <el-button type="primary">
+        <el-icon><Search /></el-icon>
+        <span> 搜索 </span>
+      </el-button>
+      <el-button type="primary">
+        <el-icon><Edit /></el-icon>
+        <span> 编辑 </span>
+      </el-button>
+      <el-button type="primary">
+        <el-icon><Delete /></el-icon>
+        <span> 删除 </span>
+      </el-button>
+    </el-button-group>
+  </el-row>
+
 </template>
 <script setup>
+
 </script>
 
