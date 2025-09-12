2b7592d3f81d77b4ffe22955f2747cff7b3a4ca5
zksks <2834969129@qq.com>
2025-09-12 14:14:27 +0800

    按钮

--- src/views/Buttons.vue
+++ src/views/Buttons.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>按钮章节</div>
-</template>
+        <el-row style="margin-bottom: 30px;">
+        <h3>按钮</h3>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+
+        </el-row>
+        <el-row style="margin-bottom: 30px;">
+        <h3>按钮属性</h3>
+        <el-button plain>朴素按钮</el-button>
+        <el-button round>圆角按钮</el-button>
+        <el-button circle>圆</el-button>
+        <el-button disabled>禁用按钮</el-button>
+        <el-button loading>加载中</el-button>
+
+        </el-row>
+        <el-row style="margin-bottom: 30px;">
+        <h3>尺寸</h3>
+        <el-button size="large">大型按钮</el-button>
+        <el-button size="medium">默认按钮</el-button>
+        <el-button >按钮</el-button>
+        <el-button size="small">小型按钮</el-button>
+        <el-button size="mini">超小按钮</el-button>
+        </el-row>
+    </template>
 <script setup>
 </script>
 
