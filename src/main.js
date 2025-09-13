78cceaca310b7f465d10f420981091373e170480
zksks <2834969129@qq.com>
2025-09-13 10:14:46 +0800

    日期选择

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import ElementPlus from 'element-plus'
 import router from './router'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
+ import zhCn from 'element-plus/dist/locale/zh-cn.mjs' 
 const app = createApp(App)
-app.use(ElementPlus)
+app.use(ElementPlus,{locale : zhCn})
 app.use(router)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
--- src/views/DatePicker.vue
+++ src/views/DatePicker.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>日期选择器章节</div>
+  <h3>日期</h3>
+  <el-date-picker v-model="date" @change="dataChanged" />
+  <h3>日期时间</h3>
+  <el-date-picker v-model="date" type="datetime" @change="dataChanged"
+  value-from="YYYY-MM-DD HH:mm:ss" 
+  ></el-date-picker>
 </template>
 <script setup>
+import { ref } from 'vue'
+const date = ref()
+const dataChanged = (value) => {
+  console.log(value)
+}
 </script>
 
