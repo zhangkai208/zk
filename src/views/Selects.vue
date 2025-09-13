62f4c5b3b801448340076dc686aff87824cd5ea9
zksks <2834969129@qq.com>
2025-09-13 09:36:30 +0800

    选择框

--- src/views/Selects.vue
+++ src/views/Selects.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>单选框、复选框章节</div>
+  <h3>单选框</h3>
+  <el-radio v-model="radio" label="1">前端</el-radio>
+  <el-radio v-model="radio" label="2" @click="radioChange">后端</el-radio>
+  <el-radio v-model="radio" label="3">服务端</el-radio>
+  <h3>单选框组</h3>
+  <el-radio-group v-model="radio2" @change="radioChange">
+    <el-radio value="a">前端</el-radio>
+    <el-radio value="b">后端</el-radio>
+    <el-radio value="c">服务端</el-radio>
+  </el-radio-group>
+
+  <h3>复选框</h3>
+  <el-checkbox-group v-model="checkbox" @change="checkboxChange">
+    <el-checkbox value="1">前端</el-checkbox>\
+    <el-checkbox value="2">后端</el-checkbox>
+    <el-checkbox value="3">服务端</el-checkbox>
+  </el-checkbox-group>
 </template>
 <script setup>
+import {ref } from 'vue'
+const checkbox = ref(['1', '2'])
+const checkboxChange = (val) => {
+  console.log(val)
+}
+const radio = ref('1')
+const radio2 = ref('a')
+const radioChange = (val) => {
+  console.log(val)
+}
 </script>
 
