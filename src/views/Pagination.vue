cbd97e6a8fceb560d7b205f3158d8675beff3f5b
zksks <2834969129@qq.com>
2025-09-13 11:11:11 +0800

    分页

--- src/views/Pagination.vue
+++ src/views/Pagination.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>分页章节</div>
+  <h3>page-size:每页显示记录数 total:总记录数</h3>
+        <el-pagination layout="prev, pager, next" :page-size="1" :total="50" />
+  <h3>background:显示背景</h3>
+        <el-pagination layout="prev, pager, next" :page-size="10" :total="50" background />
+  <h3>layout="total" 显示总数</h3>
+        <el-pagination layout="prev, pager, next,total" :page-size="10" :total="50" />
+  <h3>layout="jumper" 跳转</h3>
+        <el-pagination layout="prev, pager, next,total,jumper" :page-size="10" :total="50" @current-change="currentPage" />
 </template>
+
 <script setup>
+const currentPage = (val) => {
+            console.log("currentPage:",val)
+        }
 </script>
 
