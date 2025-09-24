efdfbc84a898134f79a4f6be8556e15cf0bb32e5
zksks <2834969129@qq.com>
2025-09-24 16:17:43 +0800

    pinia的使用

---
+++ src/store/web.js
@@ -0,0 +1,23 @@
+import {ref,reactive} from 'vue'
+import { defineStore } from 'pinia'
+//web是唯一标识符，后面是setup
+export const useWebStore = defineStore('web' , () => {
+    //定义一个响应式对象
+    const web = reactive({
+        name:'web',
+        url:'www.zk.com'
+    })
+    //定义一个响应式变量
+    const users = ref(1000)
+    //定于方法
+    const add = () => {
+        users.value++
+    
+    }
+
+    return {
+        web,
+        users,
+        add
+    }
+})
