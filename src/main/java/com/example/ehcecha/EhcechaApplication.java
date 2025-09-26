5eb8a5af1c79a1d1c32df7699758828766d217ee
zksks <2834969129@qq.com>
2025-09-26 10:07:28 +0800

    ehcache

---
+++ src/main/java/com/example/ehcecha/EhcechaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehcecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcechaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhcechaApplication.class, args);
+    }
+
+}
