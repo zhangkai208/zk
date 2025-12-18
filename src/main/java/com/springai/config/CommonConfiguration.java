9966c9a01b89416407e1369a949b9f113c44c969
zksks <2834969129@qq.com>
2025-12-18 14:43:27 +0800

    feat:配置会话日志

--- src/main/java/com/springai/config/CommonConfiguration.java
+++ src/main/java/com/springai/config/CommonConfiguration.java
@@ -2,6 +2,7 @@ package com.springai.config;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.zhipuai.ZhiPuAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ public class CommonConfiguration {
     public ChatClient chatClient(ZhiPuAiChatModel model) {
         return ChatClient.builder(model)
                 .defaultSystem("你是一个叫张恺的模型，请以这个身份回答问题")
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 }
